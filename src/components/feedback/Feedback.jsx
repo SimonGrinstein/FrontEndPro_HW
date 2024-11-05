bf5c0278b824a3a9e589e6c6a668bc180fc08f7c
SimonGrinstein <senja@web.de>
2024-11-06 00:06:49 +0100

    hw4

---
+++ src/components/feedback/Feedback.jsx
@@ -0,0 +1,45 @@
+// импорт хука useState()
+import { useState } from "react";
+import MyButton from "../../components/myButton/MyButton";
+import "./feedback.css";
+
+function Feedback() {
+ 
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+  
+
+  const likePlus = () => {
+    setLike(prev => prev + 1);
+  };
+
+  const dislikePlus = () => {
+    setDislike(prev => prev + 1);
+  };
+
+  const resetResults = () => {
+    setLike(prev => prev - like);
+    setDislike(prev => prev - dislike);
+  };
+
+
+  return (
+    <div className="lesson-container">
+      <h2>HW 04</h2>
+      <div className="counter">
+        <span>{like}</span>
+        <MyButton func={likePlus} text={"Like"} isDanger={false} />
+      </div>
+      <div className="counter">
+        <span>{dislike}</span>
+        <MyButton func={dislikePlus} text={"Dislike"} isDanger={false} />
+      </div>
+      <div className="counter">
+        <MyButton func={resetResults} text={"Reset Results"} isDanger={false} />
+      </div>
+      
+    </div>
+  );
+}
+
+export default Feedback;
--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import Homework02 from "./homeworks/homework02/Homework02";
 import Lesson03 from "./lessons/lesson03/Lesson03";
 import Homework03 from "./homeworks/homework03/Homework03";
 import Lesson04 from "./lessons/lesson04/Lesson04";
+import Homework04 from "./homeworks/homework04/Homework04";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -21,14 +22,12 @@ root.render(
     {/* не забудьте убедиться что у вас отработал импорт */}
 
     {/* <Lesson02 /> */}
-    
     {/* <Homework02/> */}
-
     {/* <Lesson03 /> */}
-
-    <Homework03 />
-    
+    {/* <Homework03 /> */}
     {/* <Lesson04 /> */}
+    <Homework04 />
+
   </>
 );
 
