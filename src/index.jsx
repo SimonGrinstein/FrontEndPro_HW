7cd4aacdcd6c9e9f6b2817938ac82e1a03340fec
SimonGrinstein <senja@web.de>
2024-11-06 21:15:13 +0100

    HW5

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Lesson01 from "./lessons/lesson01/Lesson01";
 import "./index.css";
-// импорт функции компонента
-import Lesson02 from "./lessons/lesson02/Lesson02";
-import Homework02 from "./homeworks/homework02/Homework02";
-import Lesson03 from "./lessons/lesson03/Lesson03";
-import Homework03 from "./homeworks/homework03/Homework03";
-import Lesson04 from "./lessons/lesson04/Lesson04";
-import Homework04 from "./homeworks/homework04/Homework04";
-import Lesson05 from "./lessons/lesson05/Lesson05";
+
+// import Lesson01 from "./lessons/lesson01/Lesson01";
+// import Lesson02 from "./lessons/lesson02/Lesson02";
+// import Homework02 from "./homeworks/homework02/Homework02";
+// import Lesson03 from "./lessons/lesson03/Lesson03";
+// import Homework03 from "./homeworks/homework03/Homework03";
+// import Lesson04 from "./lessons/lesson04/Lesson04";
+ import Homework04 from "./homeworks/homework04/Homework04";
+// import Lesson05 from "./lessons/lesson05/Lesson05";
+import Homework05 from "./homeworks/homework05/Homework05";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -27,9 +28,10 @@ root.render(
     {/* <Lesson03 /> */}
     {/* <Homework03 /> */}
     {/* <Lesson04 /> */}
-    {/* <Homework04 /> */}
-    <Lesson05 />
+    <Homework04 />
+    {/* <Lesson05 /> */}
+    <Homework05 />
+    
   </>
 );
-
 
